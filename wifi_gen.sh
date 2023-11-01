8bb2803f57f64842e9ccf0e7e0cf5860a50a27f6
Dan Danilyuk <DanDanilyuk@gmail.com>
2023-10-31 23:34:45 -0400

    feat: Add hidden param for network visibility
    This commit adds network visibility to the command line script and a checkbox to the HTML.

--- wifi_gen.sh
+++ wifi_gen.sh
@@ -6,8 +6,10 @@ SSID=$(networksetup -getairportnetwork en0 | awk -F': ' '{print $2}')
 # Retrieve the password
 PASSWORD=$(security find-generic-password -D "AirPort network password" -wa "$SSID")
 
-# Retrieve the security type
-SECURITY=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk -F': ' '/^ *Security/ {print $2}')
+# Retrieve the security type and if the network is hidden
+AIRPORT_INFO=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)
+SECURITY=$(echo "$AIRPORT_INFO" | awk -F': ' '/^ *Security/ {print $2}')
+HIDDEN=$(echo "$AIRPORT_INFO" | awk -F': ' '/^ *Hidden network/ {print $2}')
 
 # Adjust the security type for the QR code
 case "$SECURITY" in
@@ -17,6 +19,13 @@ case "$SECURITY" in
   *) SECURITY="WPA";;
 esac
 
-# Open Webiste
-URL="https://dandanilyuk.github.io/wifi_qr_generator/index.html?security=$SECURITY&ssid=$SSID&password=$PASSWORD"
+# Adjust the hidden network value for the QR code
+case "$HIDDEN" in
+  "Yes") HIDDEN="true";;
+  "No") HIDDEN="false";;
+  *) HIDDEN="false";;
+esac
+
+# Open Website
+URL="https://dandanilyuk.github.io/wifi_qr_generator/index.html?security=$SECURITY&ssid=$SSID&password=$PASSWORD&hidden=$HIDDEN"
 open $URL
