1c5e317dde949a122d09d060f9e0939571565a50
Dan Danilyuk <DanDanilyuk@gmail.com>
2023-11-01 00:29:05 -0400

    fix: refactor JS for simplicity

--- main.js
+++ main.js
@@ -1,44 +1,33 @@
-// Check if URL parameters are present
 const urlParams = new URLSearchParams(window.location.search);
 const securityParam = urlParams.get('security');
 const ssidParam = urlParams.get('ssid');
 const passwordParam = urlParams.get('password');
 const hiddenParam = urlParams.get('hidden');
 
-document.addEventListener('DOMContentLoaded', function() {
-  if (securityParam && ssidParam && passwordParam) {
-    // If parameters are present, hide the form and generate the QR code
-    document.getElementById('qr-form').style.display = 'none';
-
-    const wifiString = `WIFI:T:${securityParam};S:${ssidParam};P:${passwordParam};H:${hiddenParam};;`;
-
-    const qrcode = new QRCode(document.getElementById('qrcode'), {
-      text: wifiString,
-      width: 400,
-      height: 400,
-    });
-  } else {
-    // If parameters are not present, show the form
-    document.getElementById('qr-form').style.display = 'block';
-  }
-
-  document.getElementById('generate').addEventListener('click', function () {
-    const security = document.getElementById('security').value;
-    const ssid = document.getElementById('ssid').value;
-    const password = document.getElementById('password').value;
-    const hidden = document.getElementById('hidden').checked ? 'true' : 'false';
-
-    const wifiString = `WIFI:T:${security};S:${ssid};P:${password};H:${hidden};;`;
-
-    const qrcode = new QRCode(document.getElementById('qrcode'), {
-      text: wifiString,
-      width: 128,
-      height: 128,
-    });
+document.addEventListener('DOMContentLoaded', () => {
+  const qrForm = document.getElementById('qr-form');
+  const qrcode = new QRCode(document.getElementById('qrcode'), {
+    width: 400,
+    height: 400,
   });
 
-  document.getElementById('copy-command').addEventListener('click', function () {
-    const command = document.getElementById('command');
-    navigator.clipboard.writeText(command.innerText);
+  if (securityParam && ssidParam && passwordParam) {
+    qrForm.style.display = 'none';
+    const wifiString = `WIFI:T:${securityParam};S:${ssidParam};P:${passwordParam};H:${hiddenParam};;`;
+    qrcode.makeCode(wifiString);
+  } else {
+    qrForm.style.display = 'block';
+    document.getElementById('generate').addEventListener('click', () => {
+      const security = document.getElementById('security').value;
+      const ssid = document.getElementById('ssid').value;
+      const password = document.getElementById('password').value;
+      const hidden = document.getElementById('hidden').checked ? 'true' : 'false';
+      const wifiString = `WIFI:T:${security};S:${ssid};P:${password};H:${hidden};;`;
+      qrcode.makeCode(wifiString);
+    });
+  }
+
+  document.getElementById('copy-command').addEventListener('click', () => {
+    navigator.clipboard.writeText(document.getElementById('command').innerText);
   });
 });
